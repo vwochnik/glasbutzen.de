67dcbfb1a742933e347a043de09aa09b31b5c59d
Vincent Wochnik <v.wochnik@gmail.com>
2015-12-01 19:46:14 +0100

    trianglify canvas optimization

--- js/trianglify.js
+++ js/trianglify.js
@@ -1,11 +1,22 @@
 (function(wnd, $, Trianglify) {
     $(function() {
       $('.trianglify').each(function() {
-        var $elem = $(this), $canvas = undefined,
+        var $elem = $(this), $canvas = $('<canvas/>'),
             id = (((1 + Math.random()) * 0x10000)|0).toString(16).substring(1).toUpperCase(),
             xColors = ($elem.attr('data-x-colors') || "").split(/,\s*/),
             yColors = ($elem.attr('data-y-colors') || "").split(/,\s*/),
             cellSize = parseInt($elem.attr('data-cell-size') || '75');
+
+        $canvas.css({
+          position: 'absolute',
+          left: 0, top: 0,
+          right: 0, bottom: 0,
+          zIndex: -1
+        });
+        $elem.css({
+          position: 'relative',
+          background: 'none'
+        }).append($canvas);
 
         $(wnd).on('resize.trianglify-'+id, function() {
           var pattern = new Trianglify({
@@ -15,14 +26,7 @@
             x_colors: xColors,
             y_colors: yColors
           });
-          if ($canvas)
-            $canvas.remove();
-          $canvas = $(pattern.canvas()).css({
-            position: 'absolute',
-            left: 0, top: 0,
-            zIndex: -1
-          });
-          $elem.css({position: 'relative', background: 'none'}).append($canvas);
+          pattern.canvas($canvas.get(0));
         }).trigger('resize.trianglify-'+id);
       });
     });
