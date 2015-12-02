e9dd2c3aef92067416143b296719490aa3780e5a
Vincent Wochnik <v.wochnik@gmail.com>
2015-12-02 06:35:55 +0100

    seedrandom to maintain same jqcloud for dimensions

--- js/leistungen-tagcloud.js
+++ js/leistungen-tagcloud.js
@@ -3,6 +3,7 @@
     $('#leistungen-jqcloud').each(function() {
       var elem = this;
       $.getJSON("/leistungen.json", function(words) {
+        Math.seedrandom(elem.className);
         $(elem).addClass('leistungen-jqcloud').jQCloud(words, {
           autoResize: true,
           steps: 5
